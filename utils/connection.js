9f0ad74133aeb8fe3cf8a5c63fdcaac9fad84c2b
iqbalalayyubi <iqbal.is415@gmail.com>
2022-07-27 14:27:04 +0700

    connect mongo atlas

--- utils/connection.js
+++ utils/connection.js
@@ -1,2 +1,2 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://127.0.0.1:27017/app');
+mongoose.connect('mongodb+srv://iqbal:MdbiYsms7jN5TdVu@cluster0.qofpz.mongodb.net/app?retryWrites=true&w=majority');

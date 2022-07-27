4b15486d1bf3260351267448e2a2f4d83fc446ee
iqbalalayyubi <iqbal.is415@gmail.com>
2022-07-27 16:25:45 +0700

    ubah ke atlas

--- utils/connection.js
+++ utils/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 // mongo local
-mongoose.connect('mongodb://127.0.0.1:27017/app');
+// mongoose.connect('mongodb://127.0.0.1:27017/app');
 // mongo atlas
-// mongoose.connect('mongodb+srv://iqbal:MdbiYsms7jN5TdVu@cluster0.qofpz.mongodb.net/app?retryWrites=true&w=majority');
+mongoose.connect('mongodb+srv://iqbal:MdbiYsms7jN5TdVu@cluster0.qofpz.mongodb.net/app?retryWrites=true&w=majority');
